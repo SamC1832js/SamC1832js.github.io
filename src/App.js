ae6124f3f5f471dba084f5b2d66d36ce7867ebb0
sam156123328 <sam18328578207@gmail.com>
2024-10-28 00:00:23 -0500

    add fade when switch pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
-import { BrowserRouter as Router, Route, Routes, Link, Navigate } from 'react-router-dom'; // Import necessary components
+import { BrowserRouter as Router, Route, Routes, Link, Navigate } from 'react-router-dom'; 
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import React, { useState } from "react";
 import About from "./components/About";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
@@ -7,6 +9,7 @@ import Contact from "./components/Contact";
 import Home from "./components/Home";
 
 function App() {
+  const [location, setLocation] = useState(window.location.pathname);
   return (
     <Router>
       <div className="App">
@@ -17,25 +20,28 @@ function App() {
               <h1>Sam C</h1>
             </div>
             <div className="navbar-right">
-              <Link to="/home"><button>Home</button></Link>
-              <Link to="/about"><button>About</button></Link>
-              <Link to="/skills"><button>Skills</button></Link>
-              <Link to="/projects"><button>Projects</button></Link>
-              <Link to="/contact"><button>Contact</button></Link>
+              <Link to="/home" onClick={() => setLocation("/home")}><button>Home</button></Link>
+              <Link to="/about" onClick={() => setLocation("/about")}><button>About</button></Link>
+              <Link to="/skills" onClick={() => setLocation("/skills")}><button>Skills</button></Link>
+              <Link to="/projects" onClick={() => setLocation("/projects")}><button>Projects</button></Link>
+              <Link to="/contact" onClick={() => setLocation("/contact")}><button>Contact</button></Link>
             </div>
           </nav>
         </header>
 
         {/* Main content */}
         <main>
-          <Routes>
-            <Route path="/" element={<Navigate to="/home" />} /> 
-            <Route path="/home" element={<Home />} /> 
-            <Route path="/about" element={<About />} />
-            <Route path="/skills" element={<Skills />} />
-            <Route path="/projects" element={<Projects />} />
-            <Route path="/contact" element={<Contact />} /> 
-          </Routes>
+          <TransitionGroup>
+            <CSSTransition key={location} classNames="fade" timeout={300}>
+              <Routes location={location}>
+                <Route path="/home" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/skills" element={<Skills />} />
+                <Route path="/projects" element={<Projects />} />
+                <Route path="/contact" element={<Contact />} />
+              </Routes>
+            </CSSTransition>
+          </TransitionGroup>
         </main>
       </div>
     </Router>
