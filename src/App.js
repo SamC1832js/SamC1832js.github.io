f028c50fdd34b232ebaf09954c1b54e91fea0379
sam156123328 <sam18328578207@gmail.com>
2024-10-28 01:38:00 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Route, Routes, Link, Navigate } from 'react-router-dom'; 
+import { HashRouter as Router, Route, Routes, Link, Navigate } from 'react-router-dom'; 
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import React, { useState } from "react";
 import About from "./components/About";
