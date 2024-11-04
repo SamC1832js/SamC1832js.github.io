645f70fdfd4defcaf93430cb4884ae358af69b00
sam156123328 <sam18328578207@gmail.com>
2024-11-04 17:44:28 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    const timer = setTimeout(() => setLoading(false), 2500);
+    const timer = setTimeout(() => setLoading(false), 3000);
     return () => clearTimeout(timer);
   }, []);
 
